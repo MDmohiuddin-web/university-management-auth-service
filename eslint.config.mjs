85224f06dae753a5a1e5810e4d08b0fb1f3ed464
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-12-31 21:56:00 +0600

    user interface and model set

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,20 +4,27 @@ export default defineConfig({
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   env: {
     browser: true,
-    es2024: true
+    es2024: true,
   },
+  ignorePatterns: ['.eslintignore'],
+  overrides: [
+    {
+      files: ['*.js', '*.ts'],
+      // Add specific configurations if needed
+    },
+  ],
   languageOptions: {
-    globals: globals.browser
+    globals: globals.browser,
   },
   rules: {
     eqeqeq: 'off',
     'no-unused-vars': 'error',
-    'prefer-const': ['error', { ignoreReadBeforeAssign: true }]
-  }
+    'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+  },
 });
