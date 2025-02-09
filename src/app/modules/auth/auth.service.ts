95c481617852f918c3629323e5b21d6c72a78322
MOHMMED.MOHIUDDIN <149958768+MDmohiuddin-web@users.noreply.github.com>
2025-02-09 23:23:20 +0600

    working

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,37 +2,52 @@ import httpStatus from 'http-status'
 
 import ApiError from '../../../errors/ApiErrors'
 import { User } from '../users/user.model'
-import { ILoginUser } from './auth.interface'
-import jwt from 'jsonwebtoken'
+import { ILoginUser, ILoginUserResponse } from './auth.interface'
+import  { Secret } from 'jsonwebtoken'
+import { jwtHelpers } from '../../../helpes/jwtHelpers'
+import config from '../../../config'
 
-const loginUser = async (payload: ILoginUser) => {
-  const { id, password } = payload
+const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
+  const { id, password } = payload;
+  // creating instance of User
+  // const user = new User();
+  //  // access to our instance methods
+  //   const isUserExist = await user.isUserExist(id);
 
-  const isUserExist = await User.isUserExist(id)
+  const isUserExist = await User.isUserExist(id);
 
   if (!isUserExist) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist')
+    throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist');
   }
 
   if (
     isUserExist.password &&
-   ! await User.isPasswordMatched(password, isUserExist?.password)
+    !(await User.isPasswordMatched(password, isUserExist.password))
   ) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect')
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect');
   }
-  // create token
 
-  const accessToken = jwt.sign(
-    { id: isUserExist?.id, role: isUserExist?.role },
-    process.env.JWT_SECRET as string,
-    { expiresIn: process.env.JWT_EXPIRES_IN as string }
-  )
+  //create access token & refresh token
+
+  const { id: userId, role, needsPasswordChange } = isUserExist;
+  const accessToken = jwtHelpers.createToken(
+    { userId, role },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  const refreshToken = jwtHelpers.createToken(
+    { userId, role },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
 
   return {
-    accessToken
-  }
-
-}
+    accessToken,
+    refreshToken,
+    needsPasswordChange,
+  };
+};
 
 // const refreshToken = async (token: string): Promise<IRefreshTokenResponse> => {
 //   //verify token
